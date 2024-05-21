f8ffb0f13f92c2a05cfe7c9a663a19ca6786fcf4
sprill1297 <sprill1297@gmail.com>
2024-05-21 12:01:51 +0300

    Ошибка

--- public/js/fullpage.js
+++ public/js/fullpage.js
@@ -64,25 +64,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	});
   });
 
-  let logo = document.querySelector(".image");
-  let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
 
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-  });
 
-  searchBtn.addEventListener("click", ()=>{ 
-    sidebar.classList.toggle("open");
-    menuBtnChange(); 
-  });
 
-  function menuBtnChange() {
-   if(sidebar.classList.contains("open")){
-     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-   }else {
-     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-   }
-  }
+
+  
