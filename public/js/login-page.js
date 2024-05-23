a5d11931d1f9f0524d1609e292998717fe243fad
IliaGurelev <vailade.you@bk.ru>
2024-05-23 11:53:03 +0300

    Добавил логотип

--- public/js/login-page.js
+++ public/js/login-page.js
@@ -2,10 +2,13 @@ const switchBtn = document.getElementById("switch-btn");
 const swithcherForm = document.querySelector(".login-page__switcher")
 const loginForm = document.getElementById("login-form");
 const regForm = document.getElementById("reg-form");
+const mainHeaderLogo = document.getElementById("main-header-logo");
 
 switchBtn.addEventListener("click", () => {
   swithcherForm.classList.toggle("switcher--left")
   
   loginForm.classList.toggle("--transparent")
   regForm.classList.toggle("--transparent")
+
+  mainHeaderLogo.classList.toggle("main-header__logo--white")
 })
