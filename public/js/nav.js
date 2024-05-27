3737c7ab31a9bbdb963dc32658fbbb28014922bf
sprill1297 <sprill1297@gmail.com>
2024-05-27 07:53:22 +0300

    Всё готово

--- public/js/nav.js
+++ public/js/nav.js
@@ -8,51 +8,15 @@ closebtn.addEventListener("click", () => {
   nav_list.classList.remove("active");
 })
 
-const allCard = Array.from(document.querySelectorAll('.card'));
-		const container = document.querySelector('.card-wrapper');
-		const indicator = document.querySelector('.indicator');
 
-		const arrHeight = allCard.map(item=> {
-			return item.offsetHeight
-		})
-		const maxHeight = Math.max(...arrHeight);
-
-		allCard.forEach((item, idx)=> {
-			item.style.height = maxHeight + 'px';
-			item.id = 'card-' + idx;
-
-			const a = document.createElement('a');
-			a.href = '#' + item.id
-			indicator.appendChild(a);
-		})
-
-		container.style.maxHeight = maxHeight + 'px';
-
-		const allIndicator = document.querySelectorAll('.indicator a');
-
-		allIndicator[0].classList.add('active');
-
-		allIndicator.forEach(item=> {
-			item.addEventListener('click', function () {
-				allIndicator.forEach(i=> {
-					i.classList.remove('active');
-				})
-				item.classList.add('active');
-			})
-		})
-
-		container.addEventListener('scroll', function () {
-			let linkActive;
-			allCard.forEach(item=> {
-				if(this.scrollTop >= item.offsetTop - (item.offsetHeight / 2) - 28 && this.scrollTop <= (item.offsetTop + (item.offsetHeight / 2) - 28)) {
-					linkActive = item.id
-				}
-			})
-			allIndicator.forEach(item=> {
-				if(item.getAttribute('href') === ("#" + linkActive)) {
-					item.classList.add('active');
-				} else {
-					item.classList.remove('active');
-				}
-			})
-		})
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+	  if (entry.isIntersecting) {
+		entry.target.classList.add('show');
+	  } 
+	});
+  });
+const elementss = document.querySelectorAll('.Animation');
+elementss.forEach(element => observer.observe(element));
+const elementsss = document.querySelectorAll('.howtouse');
+elementss.forEach(element => observer.observe(element));
